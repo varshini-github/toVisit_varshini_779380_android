d8fa85226d5b9bfc2ca1c7c84789404cd367196d
varshini-github <vadladivarshini@gmail.com>
2020-07-18 14:52:55 +0500

    Added options to select nearby places.

--- app/src/main/java/com/toVisit_varshini_779380_android/activities/MapsActivity.java
+++ app/src/main/java/com/toVisit_varshini_779380_android/activities/MapsActivity.java
@@ -7,14 +7,18 @@ import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -27,6 +31,14 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResolvableApiException;
@@ -46,6 +58,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -57,11 +70,16 @@ import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.toVisit_varshini_779380_android.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -75,7 +93,7 @@ public class MapsActivity extends FragmentActivity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         GoogleMap.OnMarkerDragListener,
-        GoogleMap.OnMapLongClickListener {
+        GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
     private boolean cameraAnimated;
@@ -85,7 +103,11 @@ public class MapsActivity extends FragmentActivity implements
     private Marker userMarker;
     private ImageView favorite;
     private SQLiteDatabase sqLiteDatabase;
-
+    private HorizontalScrollView nearby;
+    private Button btnCafes, btnRestaurants, btnMuseums;
+    private boolean displayingNearby;
+    private Location currentLocation;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -128,9 +150,76 @@ public class MapsActivity extends FragmentActivity implements
             userMarker = mMap.addMarker(
                     markerOptions
                             .title(address));
+            userMarker.setTag("UserMarker");
             userMarker.setDraggable(true);
             showFavoriteOption();
         }
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        if (marker.getTag() != null && !marker.getTag().equals("UserMarker")) {
+            ArrayList<String> favoriteLat = new ArrayList<>();
+            ArrayList<String> favoriteLon = new ArrayList<>();
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + "FavoritePlaces", null);
+            int latColumn = cursor.getColumnIndex("Latitude");
+            int lonColumn = cursor.getColumnIndex("Longitude");
+            cursor.moveToFirst();
+
+            if (cursor != null && cursor.getCount() != 0) {
+                do {
+                    favoriteLat.add(cursor.getString(latColumn));
+                    favoriteLon.add(cursor.getString(lonColumn));
+                } while (cursor.moveToNext());
+            }
+            final String userLat = Double.toString(marker.getPosition().latitude);
+            final String userLon = Double.toString(marker.getPosition().longitude);
+            boolean sameFavoriteExists = false;
+            for (int i = 0; i < favoriteLat.size(); i++) {
+                if (favoriteLat.get(i).equals(userLat) &&
+                        favoriteLon.get(i).equals(userLon)) {
+                    sameFavoriteExists = true;
+                }
+            }
+            if (sameFavoriteExists) {
+                Toast.makeText(MapsActivity.this, "You have already marked this location as favorite", Toast.LENGTH_SHORT).show();
+            } else {
+                LayoutInflater layoutInflater = LayoutInflater.from(MapsActivity.this);
+                final View dialogView = layoutInflater.inflate(R.layout.dialog_confirmation, null);
+                final AlertDialog confirmSaveLocation = new AlertDialog.Builder(MapsActivity.this).create();
+                confirmSaveLocation.setView(dialogView);
+                confirmSaveLocation.setCancelable(false);
+
+                TextView message = dialogView.findViewById(R.id.message);
+                Button confirm_button = dialogView.findViewById(R.id.confirm_button);
+                Button cancel_button = dialogView.findViewById(R.id.cancel_button);
+
+                message.setText("Do you want to add " + marker.getTitle() + " to favorites?");
+                confirm_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        confirmSaveLocation.dismiss();
+                        sqLiteDatabase.execSQL("INSERT INTO FavoritePlaces VALUES " +
+                                "('" + marker.getTitle().replaceAll("'", "''") + "'," +
+                                "'" + marker.getPosition().latitude + "'," +
+                                "'" + marker.getPosition().longitude + "'," +
+                                "'" + "0" + "');");
+                        Toast.makeText(MapsActivity.this, "Added to favorites.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+                cancel_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        confirmSaveLocation.dismiss();
+                    }
+                });
+
+                confirmSaveLocation.show();
+            }
+        }
+
+        return true;
     }
 
     @Override
@@ -147,6 +236,7 @@ public class MapsActivity extends FragmentActivity implements
     public void onMarkerDragEnd(Marker marker) {
         String address = getAddress(new LatLng(marker.getPosition().latitude, marker.getPosition().longitude));
         userMarker = marker;
+        userMarker.setTag("UserMarker");
         userMarker.setTitle(address);
         if (userMarker.isInfoWindowShown()) {
             userMarker.showInfoWindow();
@@ -205,13 +295,76 @@ public class MapsActivity extends FragmentActivity implements
 
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS FavoritePlaces (Title VARCHAR, Latitude VARCHAR, Longitude VARCHAR, Visited VARCHAR);");
 
+        requestQueue = Volley.newRequestQueue(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        displayingNearby = false;
 
         ImageView change_type = findViewById(R.id.change_type);
         favorite = findViewById(R.id.favorite);
+        nearby = findViewById(R.id.nearby);
+        btnCafes = findViewById(R.id.cafes);
+        btnMuseums = findViewById(R.id.museums);
+        btnRestaurants = findViewById(R.id.restaurants);
+
+        nearby.setVisibility(View.VISIBLE);
+
+        btnCafes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayingNearby = true;
+                btnCafes.setBackground(getResources().getDrawable(R.drawable.bg_button_selected));
+                btnCafes.setTextColor(getResources().getColor(R.color.colorButtonSelected));
+
+                btnRestaurants.setBackground(getResources().getDrawable(R.drawable.bg_button_unselected));
+                btnRestaurants.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                btnMuseums.setBackground(getResources().getDrawable(R.drawable.bg_button_unselected));
+                btnMuseums.setTextColor(getResources().getColor(R.color.colorBlack));
+
+
+                getNearbyPlace("cafe");
+            }
+        });
+
+        btnRestaurants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayingNearby = true;
+                btnRestaurants.setBackground(getResources().getDrawable(R.drawable.bg_button_selected));
+                btnRestaurants.setTextColor(getResources().getColor(R.color.colorButtonSelected));
+
+                btnCafes.setBackground(getResources().getDrawable(R.drawable.bg_button_unselected));
+                btnCafes.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                btnMuseums.setBackground(getResources().getDrawable(R.drawable.bg_button_unselected));
+                btnMuseums.setTextColor(getResources().getColor(R.color.colorBlack));
+
+
+                getNearbyPlace("restaurant");
+            }
+        });
+
+        btnMuseums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayingNearby = true;
+                btnMuseums.setBackground(getResources().getDrawable(R.drawable.bg_button_selected));
+                btnMuseums.setTextColor(getResources().getColor(R.color.colorButtonSelected));
+
+                btnCafes.setBackground(getResources().getDrawable(R.drawable.bg_button_unselected));
+                btnCafes.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                btnRestaurants.setBackground(getResources().getDrawable(R.drawable.bg_button_unselected));
+                btnRestaurants.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                getNearbyPlace("museum");
+
+            }
+        });
+
 
         change_type.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -293,7 +446,6 @@ public class MapsActivity extends FragmentActivity implements
                     if (favoriteLat.get(i).equals(userLat) &&
                             favoriteLon.get(i).equals(userLon)) {
                         sameFavoriteExists = true;
-                        return;
                     }
                 }
                 if (sameFavoriteExists) {
@@ -340,12 +492,74 @@ public class MapsActivity extends FragmentActivity implements
 
     }
 
+    private void getNearbyPlace(String category) {
+
+        mMap.clear();
+        if (userMarker != null) {
+            MarkerOptions markerOptions = new MarkerOptions().position(userMarker.getPosition());
+            userMarker = mMap.addMarker(markerOptions.title(userMarker.getTitle()));
+            userMarker.setTag("UserMarker");
+        }
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+
+        url = url + "location=" + currentLocation.getLatitude() + "," + currentLocation.getLongitude();
+
+        url = url + "&radius=1000&type=" + category + "&key=" + getString(R.string.google_maps_key);
+
+        System.out.println("Clicked: " + url);
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject jsonObject;
+                        JSONArray jResults;
+
+                        String latitude = "";
+                        String longitude = "";
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                        String name = sdf.format(new Date());
+
+                        try {
+                            jsonObject = new JSONObject(response);
+                            jResults = jsonObject.getJSONArray("results");
+
+
+                            for (int i = 0; i < jResults.length(); i++) {
+                                if (jResults.getJSONObject(i).getString("name") != null || !jResults.getJSONObject(i).getString("name").equals("")) {
+                                    name = jResults.getJSONObject(i).getString("name");
+                                }
+                                latitude = jResults.getJSONObject(i).getJSONObject("geometry").getJSONObject("location").getString("lat");
+                                longitude = jResults.getJSONObject(i).getJSONObject("geometry").getJSONObject("location").getString("lng");
+                                mMap.addMarker(new MarkerOptions()
+                                        .position(new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude)))
+                                        .title(name)).setTag("Nearby");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MapsActivity.this, "There was an error getting nearby.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+        requestQueue.add(stringRequest);
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         cameraAnimated = false;
         mMap.setOnMapLongClickListener(this);
         mMap.setOnMarkerDragListener(this);
+        mMap.setOnMarkerClickListener(this);
 
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
 
@@ -408,6 +622,7 @@ public class MapsActivity extends FragmentActivity implements
                             markerOptions
                                     .title(address));
                     userMarker.setDraggable(true);
+                    userMarker.setTag("UserMarker");
                     showFavoriteOption();
                 }
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 15));
@@ -468,6 +683,7 @@ public class MapsActivity extends FragmentActivity implements
                 if (locationResult == null) {
                     return;
                 }
+                currentLocation = locationResult.getLocations().get(0);
                 if (!cameraAnimated) {
                     cameraAnimated = true;
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
@@ -480,12 +696,33 @@ public class MapsActivity extends FragmentActivity implements
 
     @Override
     public void onBackPressed() {
-        if (userMarker != null) {
+
+        if (displayingNearby) {
+
+            btnCafes.setBackground(getResources().getDrawable(R.drawable.bg_button_unselected));
+            btnCafes.setTextColor(getResources().getColor(R.color.colorBlack));
+
+            btnMuseums.setBackground(getResources().getDrawable(R.drawable.bg_button_unselected));
+            btnMuseums.setTextColor(getResources().getColor(R.color.colorBlack));
+
+            btnRestaurants.setBackground(getResources().getDrawable(R.drawable.bg_button_unselected));
+            btnRestaurants.setTextColor(getResources().getColor(R.color.colorBlack));
+
             mMap.clear();
-            userMarker = null;
-            hideFavoriteOption();
+            displayingNearby = false;
+            if (userMarker != null) {
+                MarkerOptions markerOptions = new MarkerOptions().position(userMarker.getPosition());
+                userMarker = mMap.addMarker(markerOptions.title(userMarker.getTitle()));
+                userMarker.setTag("UserMarker");
+            }
         } else {
-            super.onBackPressed();
+            if (userMarker != null) {
+                mMap.clear();
+                userMarker = null;
+                hideFavoriteOption();
+            } else {
+                super.onBackPressed();
+            }
         }
     }
 }
